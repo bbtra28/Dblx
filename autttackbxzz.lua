f51ee83da54351c85894626befe2fc5f8fb116f4
bbtra28 <132905415+bbtra28@users.noreply.github.com>
2025-10-04 18:11:57 +0700

    Update autttackbxzz.lua

--- autttackbxzz.lua
+++ autttackbxzz.lua
@@ -1,3 +1,11 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local names = {}
+for i,v in pairs(ReplicatedStorage:GetChildren()) do
+    if v:IsA("RemoteEvent") or v:IsA("RemoteFunction") then
+        table.insert(names, v.Name)
+    end
+end
+
 local player = game.Players.LocalPlayer
 local gui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
 local label = Instance.new("TextLabel", gui)
@@ -5,10 +13,4 @@ label.Size = UDim2.new(1, 0, 0, 50)
 label.BackgroundTransparency = 0.5
 label.TextScaled = true
 label.TextColor3 = Color3.new(1,1,1)
-label.BackgroundColor3 = Color3.new(0,0,0)
-
-local names = {}
-for i,v in pairs(workspace:GetChildren()) do
-    table.insert(names, v.Name)
-end
-label.Text = table.concat(names, " | ")
+label
