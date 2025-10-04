0b57ebc9a440931fc09d16c2a0aca8d75a134e6c
bbtra28 <132905415+bbtra28@users.noreply.github.com>
2025-10-04 18:03:28 +0700

    Update autttackbxzz.lua

--- autttackbxzz.lua
+++ autttackbxzz.lua
@@ -1,5 +1,14 @@
-for _, v in pairs(workspace:GetDescendants()) do
-    if v:IsA("Model") and v:FindFirstChild("Humanoid") then
-        print("Ditemukan model dengan Humanoid:", v.Name, "dalam folder:", v.Parent.Name)
-    end
+local player = game.Players.LocalPlayer
+local gui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
+local label = Instance.new("TextLabel", gui)
+label.Size = UDim2.new(1, 0, 0, 50)
+label.BackgroundTransparency = 0.5
+label.TextScaled = true
+label.TextColor3 = Color3.new(1,1,1)
+label.BackgroundColor3 = Color3.new(0,0,0)
+
+local names = {}
+for i,v in pairs(workspace:GetChildren()) do
+    table.insert(names, v.Name)
 end
+label.Text = table.concat(names, " | ")
